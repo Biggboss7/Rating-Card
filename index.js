ffc7b4f433d18fbca32fc36006193a4ec142d3e4
Bigboss <mtse431@gmail.com>
2024-02-07 10:17:08 +1030

    Completes Refactor

--- index.js
+++ index.js
@@ -1,38 +1,88 @@
-const listButtonRate = document.querySelectorAll(".rate-btn");
-const submitButton = document.querySelector(".submit-btn");
-const rateResult = document.querySelector(".rate-text");
+"use strict";
 
-let previousBtn = null;
-let givenRate;
+// Target Elements
+const ratingFormEl = document.getElementById("ratingForm");
+const feedbackCardEl = document.querySelector(".feedback--card");
+const rateGivenEl = document.getElementById("rateGiven");
+const inputRateContainer = document.querySelector(".input--rates__container");
+const submitButton = document.querySelector(".btn--submit");
 
-const eventAssignment = function(button) {
-  button.addEventListener("click", selected);
-}
+const ratingApp = {
+  currSelectedRateBtn: null,
 
-const selected = function () {
-  if (previousBtn === null) {
-    previousBtn = Number(this.innerHTML) - 1;
-  } else {
-      listButtonRate[previousBtn].classList.remove("selected-button");
-      previousBtn = Number(this.innerHTML) - 1;
+  prevSelectedRateBtn: null,
+
+  _updateCurrPrevRateBtn(element) {
+    this.prevSelectedRateBtn = this.currSelectedRateBtn;
+    this.currSelectedRateBtn = element;
+  },
+
+  _markCurrSelectedRateBtn() {
+    this.currSelectedRateBtn.classList.add("selected");
+  },
+
+  _unmarkPrevSelectedRateBtn() {
+    if (!this.prevSelectedRateBtn) return;
+
+    this.prevSelectedRateBtn.classList.remove("selected");
+  },
+
+  _rateSelectionLogic(e) {
+    if (!e.target.closest(".input--rate")) return;
+
+    e.preventDefault();
+
+    const entry = e.target.closest(".input--rate");
+
+    if (entry.classList.contains("selected")) return;
+
+    this._updateCurrPrevRateBtn(entry);
+    this._markCurrSelectedRateBtn();
+    this._unmarkPrevSelectedRateBtn();
+
+    submitButton.focus();
+  },
+
+  _displayFeedback() {
+    ratingFormEl.classList.toggle("hidden");
+    feedbackCardEl.classList.toggle("hidden");
+  },
+
+  _assignRate() {
+    const rateValue = this.currSelectedRateBtn.querySelector(
+      "input[type='radio']"
+    ).value;
+    rateGivenEl.textContent = rateValue;
+  },
+
+  _submitFormProcess(e) {
+    e.preventDefault();
+
+    if (!this.currSelectedRateBtn) {
+      alert("Your Feedback will help us to improve...");
+      return;
     }
-    this.classList.add("selected-button");
-    givenRate = this.innerHTML;
+
+    this._assignRate();
+
+    this._displayFeedback();
+  },
+
+  _submitFormByKeyboard(e) {
+    if (e.key !== "Enter") return;
+
+    this._submitFormProcess(e);
+  },
+
+  init() {
+    // Apply Event Delegation
+    inputRateContainer.addEventListener(
+      "click",
+      this._rateSelectionLogic.bind(this)
+    );
+
+    ratingFormEl.addEventListener("submit", this._submitFormProcess.bind(this));
+  },
 };
 
-const submission = function () {
-  if (givenRate === undefined) {
-    alert("Please give us any rate")
-  } else {
-      document.querySelector(".before-submit").style.display = "none";
-      document.querySelector(".after-submit").style.display = "block";
-      displayResult();
-    }
-};
-
-const displayResult = function () {
-  rateResult.innerHTML = `You selected ${givenRate} out of 5`
-};
-
-listButtonRate.forEach(eventAssignment);
-submitButton.addEventListener("click", submission);
+ratingApp.init();
