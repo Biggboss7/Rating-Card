29ddfaa25a6392483b57a5d8a9b820cafa233784
Bigboss <mtse431@gmail.com>
2024-02-08 10:08:00 +1030

    Disables input radio selected by JS

--- index.js
+++ index.js
@@ -10,48 +10,13 @@ const submitButton = document.querySelector(".btn--submit");
 const ratingApp = {
   currSelectedRateBtn: null,
 
-  prevSelectedRateBtn: null,
-
-  _updateCurrPrevRateBtn(element) {
-    this.prevSelectedRateBtn = this.currSelectedRateBtn;
-    this.currSelectedRateBtn = element;
-  },
-
-  _markCurrSelectedRateBtn() {
-    this.currSelectedRateBtn.classList.add("selected");
-  },
-
-  _unmarkPrevSelectedRateBtn() {
-    if (!this.prevSelectedRateBtn) return;
-
-    this.prevSelectedRateBtn.classList.remove("selected");
-  },
-
-  _rateSelectionLogic(e) {
-    if (!e.target.closest(".input--rate")) return;
-
-    e.preventDefault();
-
-    const entry = e.target.closest(".input--rate");
-
-    if (entry.classList.contains("selected")) return;
-
-    this._updateCurrPrevRateBtn(entry);
-    this._markCurrSelectedRateBtn();
-    this._unmarkPrevSelectedRateBtn();
-
-    submitButton.focus();
-  },
-
   _displayFeedback() {
     ratingFormEl.classList.toggle("hidden");
     feedbackCardEl.classList.toggle("hidden");
   },
 
   _assignRate() {
-    const rateValue = this.currSelectedRateBtn.querySelector(
-      "input[type='radio']"
-    ).value;
+    const rateValue = this.currSelectedRateBtn.value;
     rateGivenEl.textContent = rateValue;
   },
 
@@ -59,7 +24,7 @@ const ratingApp = {
     e.preventDefault();
 
     if (!this.currSelectedRateBtn) {
-      alert("Your Feedback will help us to improve...");
+      alert("Please help us to improve by giving a rate... 🙏");
       return;
     }
 
@@ -68,21 +33,18 @@ const ratingApp = {
     this._displayFeedback();
   },
 
-  _submitFormByKeyboard(e) {
-    if (e.key !== "Enter") return;
-
-    this._submitFormProcess(e);
-  },
-
   init() {
     // Apply Event Delegation
     inputRateContainer.addEventListener(
       "click",
-      this._rateSelectionLogic.bind(this)
+      function (e) {
+        if (!e.target.classList.contains("input--rate")) return;
+        this.currSelectedRateBtn = e.target;
+      }.bind(this)
     );
 
     ratingFormEl.addEventListener("submit", this._submitFormProcess.bind(this));
   },
 };
 
-// ratingApp.init();
+ratingApp.init();
